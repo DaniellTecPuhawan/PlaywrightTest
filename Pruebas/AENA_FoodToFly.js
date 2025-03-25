5b8374f01bcb564cf5d36bfc95151ab326343ed0
DaniellTecPuhawan <carlosdanielltecpuhawan@gmail.com>
2025-03-25 13:42:55 +0100

    AENA FoodToFly

--- Pruebas/AENA_FoodToFly.js
+++ Pruebas/AENA_FoodToFly.js
@@ -1,27 +1,72 @@
-const { chromium } = require('playwright'); // Solo utilizamos Chromium (Chrome) y Edge
+const { chromium } = require('playwright');
 const fs = require('fs');
+
+const createTimestampFolder = () => {
+  const timestamp = new Date();
+  const hour = timestamp.getHours().toString().padStart(2, '0');
+  const minute = timestamp.getMinutes().toString().padStart(2, '0');
+  const second = timestamp.getSeconds().toString().padStart(2, '0');
+  const day = timestamp.getDate().toString().padStart(2, '0');
+  const month = (timestamp.getMonth() + 1).toString().padStart(2, '0');
+  const year = timestamp.getFullYear();
+
+  const folderName = `${hour}-${minute}-${second}-${day}-${month}-${year}`;
+  const folderPath = `../screenshots/AENA_FoodToFly/${folderName}`;
+
+  // Crear la carpeta si no existe
+  if (!fs.existsSync(folderPath)) {
+    fs.mkdirSync(folderPath, { recursive: true });
+  }
+
+  return folderPath;
+};
+
+// Se realizan capturas del formulario sin rellenar
+const folderPath = createTimestampFolder();
 
 // Crea un log donde se registre la automatización
 const logToFile = (message) => {
-  const timestamp = new Date().toISOString();
-  fs.appendFileSync('../logs/AENA_FoodToFly_log.txt', `[${timestamp}] ${message}\n`);
+  const timestamp = new Date();
+  const formattedDate = `${timestamp.getDate().toString().padStart(2, '0')}/${(timestamp.getMonth() + 1).toString().padStart(2, '0')}/${timestamp.getFullYear()}`;
+  const formattedTime = `${timestamp.getHours().toString().padStart(2, '0')}:${timestamp.getMinutes().toString().padStart(2, '0')}`;
+  const logMessage = `[${formattedDate} - ${formattedTime}] ${message}\n`;
+  fs.appendFileSync('../logs/AENA_FoodToFly_log.txt', logMessage); // Aquí se mantiene tu ruta original
 };
 
-// Datos guardados en variables
-const email = 'daniell.tec@entelgy.com';
-const password = 'Arbust0@01';
+// Cargar el primer usuario desde el archivo JSON
+const loadUsers = () => {
+  const rawData = fs.readFileSync('../utils/users.json');
+  const users = JSON.parse(rawData);
+  return users[0];  // Usamos solo el primer usuario
+};
+
+const user = loadUsers(); // Cargar el primer usuario
+
+// Función para verificar el estado de la respuesta de la página
+const checkResponseStatus = async (page, url) => {
+  const response = await page.goto(url);
+  if (response.status() !== 200) {
+    console.error(`Error: La página no se cargó correctamente. Código de estado: ${response.status()}`);
+    logToFile(`Error: La página no se cargó correctamente. Código de estado: ${response.status()}`);
+    throw new Error(`La página no se cargó correctamente. Código de estado: ${response.status()}`);
+  } else {
+    console.log(`Página cargada con éxito: ${url}`);
+    logToFile(`Página cargada con éxito: ${url}`);
+  }
+};
 
 (async () => {
+  let chromeBrowser, edgeBrowser, chromePage, edgePage;
+
   try {
-    console.log('Iniciando AENA_Travel_Web_Windows');
+    console.log('Iniciando login AENA_Club');
 
     // Abre el script solo en Chrome y Edge en modo gráfico (sin headless)
-    const [chromeBrowser, edgeBrowser] = await Promise.all([
+    [chromeBrowser, edgeBrowser] = await Promise.all([
       chromium.launch({
         headless: false,  // Modo gráfico (con interfaz visible)
         slowMo: 100
       }),
-
       chromium.launch({
         channel: 'msedge', // Para Microsoft Edge
         headless: false,  // Modo gráfico (con interfaz visible)
@@ -33,26 +78,10 @@ const password = 'Arbust0@01';
     const chromeContext = await chromeBrowser.newContext();
     const edgeContext = await edgeBrowser.newContext();
 
-    const [chromePage, edgePage] = await Promise.all([
+    [chromePage, edgePage] = await Promise.all([
       chromeContext.newPage(),
       edgeContext.newPage()
     ]);
-
-    // Interceptar la respuesta y verificar el código de estado
-    const checkResponseStatus = async (page, url) => {
-      const [response] = await Promise.all([
-        page.waitForResponse(response => response.url() === url && response.status() === 200),
-        page.goto(url, { waitUntil: 'domcontentloaded', timeout: 120000 })
-      ]);
-
-      if (response.status() === 200) {
-        console.log(`Respuesta 200 OK para la URL: ${url}`);
-        logToFile(`Respuesta 200 OK para la URL: ${url}`);
-      } else {
-        console.log(`Respuesta inesperada: ${response.status()} para la URL: ${url}`);
-        logToFile(`Respuesta inesperada: ${response.status()} para la URL: ${url}`);
-      }
-    };
 
     // Verificar el estado de la respuesta para Chrome y Edge
     await checkResponseStatus(chromePage, 'https://foodtofly.aena.es/es/');
@@ -69,36 +98,41 @@ const password = 'Arbust0@01';
     await chromePage.click('#topNavigation > div.header-links > div > ul > li:nth-child(3) > a');
     await edgePage.click('#topNavigation > div.header-links > div > ul > li:nth-child(3) > a');
 
-    await chromePage.screenshot({ path: 'screenshot_formulario1.png' });
-    await edgePage.screenshot({ path: 'screenshot_formulario2.png' });
+    
+
+    await chromePage.waitForTimeout(3000);
+    await edgePage.waitForTimeout(3000);
+
+    // Tomar las capturas de pantalla y guardarlas en la carpeta creada
+    await chromePage.screenshot({ path: `${folderPath}/screenshot_formulario_chrome.png` });
+    await edgePage.screenshot({ path: `${folderPath}/screenshot_formulario_edge.png` });
 
     console.log('Navegando en Chrome');
     logToFile('Navegando en Chrome');
     console.log('Navegando en Edge');
     logToFile('Navegando en Edge');
-
    
     // Llenar los campos de login (correo y contraseña) y enviar el formulario
-    await chromePage.fill('#gigya-login-form .gigya-input-text', email);
-    await edgePage.fill('#gigya-login-form .gigya-input-text', email);
+    await chromePage.fill('#gigya-login-form .gigya-input-text', user.email);  // Usar el primer usuario
+    await edgePage.fill('#gigya-login-form .gigya-input-text', user.email);    // Usar el primer usuario
 
     console.log('Correo ingresado en Chrome');
     logToFile('Correo ingresado en Chrome');
     console.log('Correo ingresado en Edge');
     logToFile('Correo ingresado en Edge');
 
-    await chromePage.fill('#gigya-login-form .gigya-input-password', password);
-    await edgePage.fill('#gigya-login-form .gigya-input-password', password);
+    await chromePage.fill('#gigya-login-form .gigya-input-password', user.password);  // Usar el primer password
+    await edgePage.fill('#gigya-login-form .gigya-input-password', user.password);    // Usar el primer password
 
     console.log('Contraseña ingresada en Chrome');
     logToFile('Contraseña ingresada en Chrome');
     console.log('Contraseña ingresada en Edge');
     logToFile('Contraseña ingresada en Edge');
 
-    await chromePage.waitForTimeout(5000);
-    await edgePage.waitForTimeout(5000);
-
-
+    // Se realizan capturas del formulario rellenado
+    await chromePage.screenshot({ path: `${folderPath}/screenshot_formulario_rellenado_chrome.png` });
+    await edgePage.screenshot({ path: `${folderPath}/screenshot_formulario_rellenado_edge.png` });
+    
     // Hacer clic en el botón de submit
     const chromeSubmitButton = await chromePage.locator('#gigya-login-form .gigya-input-submit');
     const edgeSubmitButton = await edgePage.locator('#gigya-login-form .gigya-input-submit');
@@ -127,22 +161,52 @@ const password = 'Arbust0@01';
     await chromePage.waitForTimeout(3000);
     await edgePage.waitForTimeout(3000);
 
+    // Se accede al menú del usuario para comprobar que ha iniciado sesión correctamente
+    await chromePage.waitForSelector('#topNavigation > div.header-links > div > ul > li:nth-child(2) > a', { timeout: 15000 });
+    await edgePage.waitForSelector('#topNavigation > div.header-links > div > ul > li:nth-child(2) > a', { timeout: 15000 });
+   
+    await chromePage.click('#topNavigation > div.header-links > div > ul > li:nth-child(2) > a');
+    await edgePage.click('#topNavigation > div.header-links > div > ul > li:nth-child(2) > a');
 
-    await chromePage.check('#gigya-checkbox-31606798240953280');
-    await edgePage.check('#gigya-checkbox-31606798240953280');
-    
-    await chromePage.click('#gigya-profile-form > div:nth-child(3) > div.gigya-composite-control.gigya-composite-control-submit.is-centered.is-aena-green > input');
+    console.log('Se accede al menú del usuario');
+    logToFile('Se accede al menú del usuario');
+   
+    await chromePage.waitForSelector('#gigya-profile-form > div.gigya-layout-row.with-two-inputs > div:nth-child(1) > div > h2.gigya-composite-control.gigya-composite-control-header.aena-form-header', { timeout: 15000 });
+    await edgePage.waitForSelector('#gigya-profile-form > div.gigya-layout-row.with-two-inputs > div:nth-child(1) > div > h2.gigya-composite-control.gigya-composite-control-header.aena-form-header', { timeout: 15000 });
+   
+    const chromeText = await chromePage.textContent('#gigya-profile-form > div.gigya-layout-row.with-two-inputs > div:nth-child(1) > div > h2.gigya-composite-control.gigya-composite-control-header.aena-form-header');
+    const edgeText = await edgePage.textContent('#gigya-profile-form > div.gigya-layout-row.with-two-inputs > div:nth-child(1) > div > h2.gigya-composite-control.gigya-composite-control-header.aena-form-header');
+   
+    if (chromeText?.includes('Información personal')) {
+      await chromePage.screenshot({ path: `${folderPath}/screenshot_sesion_iniciada_chrome.png` });
+      console.log('Sesión Iniciada con éxito');
+      logToFile('Login Válido');
+    } else {
+      await chromePage.screenshot({ path: `${folderPath}/screenshot_sesion_erronea_chrome.png` });
+      console.log('Sesión No Iniciada, inténtelo denuevo');
+      logToFile('Login Inválido');
+    }
+   
+    if (edgeText?.includes('Información personal')) {
+      await edgePage.screenshot({ path: `${folderPath}/screenshot_sesion_iniciada_edge.png` });
+      console.log('Sesión Iniciada con éxito');
+      logToFile('Login Válido');
+    } else {
+      await edgePage.screenshot({ path: `${folderPath}/screenshot_sesion_erronea_edge.png` });
+      console.log('Sesión No Iniciada, inténtelo denuevo');
+      logToFile('Login Inválido');
+    }
 
-    await edgePage.click('#gigya-profile-form > div:nth-child(3) > div.gigya-composite-control.gigya-composite-control-submit.is-centered.is-aena-green > input');
+    console.log('Login completada en Chrome');
+    logToFile('Login completada en Chrome');
+    console.log('Login completada en Edge');
+    logToFile('Login completada en Edge');
 
-    console.log('Navegación completada en Chrome');
-    logToFile('Navegación completada en Chrome');
-    console.log('Navegación completada en Edge');
-    logToFile('Navegación completada en Edge');
+    // Cerrar los navegadores al finalizar el test
+    await chromeBrowser.close();
+    await edgeBrowser.close();
 
-    // Mantener los navegadores abiertos
-    await new Promise(() => {});
-    console.log('Automatización AENA_Travel_Web_Windows realizada');
+    console.log('Login en AENA FoodToFly realizada');
   } catch (error) {
     console.error(`Error al ejecutar el script: ${error.message}`);
     logToFile(`Error al ejecutar el script: ${error.message}`);
